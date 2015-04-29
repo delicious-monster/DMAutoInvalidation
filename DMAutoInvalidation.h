8480f788cf5946e8d51bd73f018d26110757d5ff
Jonathon Mah <me@JonathonMah.com>
2015-04-29 14:02:16 -0700

    Add Apache license, and sanitize source headers
    Project is "DMAutoInvalidation".

--- DMAutoInvalidation.h
+++ DMAutoInvalidation.h
@@ -1,9 +1,18 @@
 //
 //  DMAutoInvalidation.h
-//  Library
+//  DMAutoInvalidation
 //
 //  Created by Jonathon Mah on 2012-01-21.
-//  Copyright (c) 2012 Delicious Monster Software. All rights reserved.
+//  Copyright (c) 2012 Delicious Monster Software.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import <Foundation/Foundation.h>
--- LIFilesystemEventObserver.m
+++ LIFilesystemEventObserver.m
@@ -1,9 +1,18 @@
 //
 //  LIFilesystemEventObserver.m
-//  Library
+//  DMAutoInvalidation
 //
-//  Created by William Shipley on 1/3/07.
-//  Copyright 2007 Delicious Monster Software. All rights reserved.
+//  Created by William Shipley on 2007-01-03.
+//  Copyright (c) 2007 Delicious Monster Software.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import "LIFilesystemEventObserver.h"
